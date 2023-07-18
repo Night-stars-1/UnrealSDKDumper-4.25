57bd643cb023c881146bd565b5ddb9505a6b05e1
renbohan <1551608379@qq.com>
2023-07-18 17:41:56 +0800

    add unreal engine types to the nodes

--- Dumper/RefGraphSolver.h
+++ Dumper/RefGraphSolver.h
@@ -37,13 +37,34 @@ class RefGraphSolver
 
   // map the TypeName -> PackageName
   static std::unordered_map<std::string, std::string> typeDefMap;
+
+  // map the packageName -> NodePointer
   static std::unordered_map<std::string, Node*> nodesMap;
+
+  // Store all the package nodes for topo sort
   static std::vector<Node*> packageNodes;
 
-  
+  // 硬件自带的类型定义，需要手动获取写上去的
+  static std::vector<std::string> BasicTypes;
+  static std::vector<std::string> CoreUObject;
+
+
+  static void PushNode(std::string packageName) {
+    auto newNode = new Node(packageName);
+    nodesMap[packageName] = newNode;
+    packageNodes.push_back(newNode);
+  }
 
   static void LoadUnrealPackageDef() {
     // 加载虚幻引擎自带的一些类的定义，这些定义不是从游戏中dump的，是从引擎代码中抠出来的，需要单独加载
+    for (auto ClassName : BasicTypes) {
+      typeDefMap[ClassName] = "BasicTypes";
+    }
+    for (auto ClassName : CoreUObject) {
+      typeDefMap[ClassName] = "CoreUObject";
+    }
+    PushNode("BasicTypes");
+    PushNode("CoreUObject");
   }
 
   static void LoadPackageDef(UE_UPackage& package) {
@@ -59,10 +80,7 @@ class RefGraphSolver
       typeDefMap[enums.EnumName] = packageName;
     }
 
-    auto newNode = new Node(packageName);
-    nodesMap[packageName] = newNode;
-    packageNodes.push_back(newNode);
-
+    PushNode(packageName);
 
     if (verboseDebug) {
       printf("Loaded all packages defs!");
---
+++ tools/ClassNameParser.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::string extractClassName(const std::string& input) {
+    size_t pos = input.find("class");
+    if (pos != std::string::npos) { // "class" found in the input string
+        pos += 5; // Move to the character right after "class"
+
+        // Find the next colon or newline character from the position of "class"
+        size_t colonPos = input.find(":", pos);
+        size_t newlinePos = input.find("\n", pos);
+        
+        if (colonPos != std::string::npos && newlinePos != std::string::npos) {
+            // Return the content between "class" and the next colon
+            if (colonPos < newlinePos)
+                return input.substr(pos, colonPos - pos);
+            // Return the content between "class" and the next newline
+            else
+                return input.substr(pos, newlinePos - pos);
+        }
+        else if (colonPos != std::string::npos) {
+            // Return the content between "class" and the next colon
+            return input.substr(pos, colonPos - pos);
+        }
+        else if (newlinePos != std::string::npos) {
+            // Return the content between "class" and the next newline
+            return input.substr(pos, newlinePos - pos);
+        }
+    }
+
+    // If "class" was not found or no content between "class" and the next colon or newline
+    return "";
+}
+
+std::string trimWhitespace(const std::string& input) {
+    size_t start = 0;
+    size_t end = input.length() - 1;
+
+    // Find the first non-space character from the beginning of the string
+    while (start <= end && std::isspace(input[start])) {
+        start++;
+    }
+
+    // Find the last non-space character from the end of the string
+    while (end >= start && std::isspace(input[end])) {
+        end--;
+    }
+
+    // Return the trimmed substring
+    return input.substr(start, end - start + 1);
+}
+
+vector<string> classnames;
+
+ifstream input("input.h");
+
+ofstream output("result.cpp");
+
+
+int main(){
+	if(!input) return -1;
+	string linecontent;
+	while(getline(input, linecontent)) {
+		if(linecontent.find("template") != string::npos) continue;
+		if(linecontent.find("friend") != string::npos) continue;
+		if(linecontent.find("//") != string::npos) continue;
+		if(linecontent.find("(") != string::npos) continue;
+		
+		string res = extractClassName(linecontent + "\n"); 
+		if(res != "") {
+			classnames.push_back(trimWhitespace(res));
+			cout << "origin: " << linecontent << endl;
+			cout << "res: " << res << endl;
+		}
+	}
+	output << "std::vector<std::string> RefGraphSolver::BasicTypes = {" << endl;
+	for(auto name : classnames) {
+		cout << "name: " << name << endl;
+		output << "\t\"" << name << "\"," << endl; 
+	}
+	output << "};";
+	return 0;
+}
+
