8b15b3edde754574b60870634720ddf866522e4a
renbohan <1551608379@qq.com>
2023-07-22 22:56:23 +0800

    fix classsizefixer bugs that will make some classes disappear

--- Dumper/wrappers.cpp
+++ Dumper/wrappers.cpp
@@ -1190,12 +1190,6 @@ std::unordered_map<std::string, int> typeDefCnt;
 
 void UE_UPackage::GenerateStruct(UE_UStruct object, std::vector<Struct>& arr, bool findPointers) {
   Struct s;
-  if (object.GetCppName() == "FSMConnectionValidator") {
-    _CrtDbgBreak();
-  }
-  if (object.GetCppName() == "FSMTextDisplayWidgetInfo") {
-    _CrtDbgBreak();
-  }
   //s.Size = object.GetSize();
   if(ClassSizeFixer::sizeMp.count(object.GetAddress()))
     s.Size = ClassSizeFixer::sizeMp[object.GetAddress()]; // Fix the class
