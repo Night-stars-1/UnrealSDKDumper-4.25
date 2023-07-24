04cf98a4a836fb7ff10a09004b1191a539c1840a
renbohan <1551608379@qq.com>
2023-07-24 13:07:30 +0800

    Add github CI

---
+++ build.ps1
@@ -0,0 +1,16 @@
+param([string]$buildtfm = 'x64')
+
+$build64	 = $buildtfm -eq 'x64'
+$build86	 = $buildtfm -eq 'x86'
+
+$msbuildPath = Split-Path (& "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.Component.MSBuild -find MSBuild\Current\Bin\amd64\MSBuild.exe | Select-Object -First 1) -Parent
+Write-Host "VisualStudioCompiler:" + $msbuildPath
+$env:PATH = $msbuildPath + ';' + $env:PATH
+
+if($build86){
+    msbuild Dumper/Dumper.vcxproj /p:Platform=x86 /p:Configuration=Release
+}
+
+if($build64){
+    msbuild Dumper/Dumper.vcxproj /p:Platform=x64 /p:Configuration=Release
+}
